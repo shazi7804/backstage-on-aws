28f8be34eb32009510536e7ab92c84776666372a
shazi7804 <shazi7804@gmail.com>
2024-02-23 16:01:49 +0800

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+export AWS_REGION=your-region
+export AWS_ACCOUNT=your-account-id
+export GITHUB_TOKEN=your-github-token
+export AUTH_GITHUB_CLIENT_ID=your-github-auth-app-client-id
+export AUTH_GITHUB_CLIENT_SECRET=your-github-auth-app-client-secret
+export ECR_REPO_NAME=backstage
+export ECR_REPO_URI="${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO_NAME}:latest"
+
+# Toolkit setup
+## Install AWS CDK and YARN
+npm install -g aws-cdk yarn
+cdk --version
+
+## Install Kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.28.5/2024-01-04/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo install -m 555 kubectl /usr/local/bin/kubectl
+rm kubectl
+kubectl version --client
+
+## Install Argocd
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+argocd version
+
+
+# Build Backstage infra and application
+git clone https://github.com/shazi7804/backstage-on-aws
+
+## Application
+
+cd backstage/ && yarn install
+yarn build:backend --config ../../app-config.yaml
+
+# Build backstage image
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin ${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com
+aws ecr create-repository \
+    --repository-name $ECR_REPO_NAME \
+    --region $AWS_REGION
+
+DOCKER_BUILDKIT=1 docker build . -t backstage
+docker tag backstage $ECR_REPO
+docker buildx build \
+    --platform linux/amd64,linux/arm64 \
+    --push \
+    --tag $ECR_REPO \
+    .
+
+## Infra deploy
+cd ../../ && yarn install
+cdk bootstrap
+cdk deploy BackstageInfraStack
+
